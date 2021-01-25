317516921d72f3bd88a1add6b3a6671f0c145110
Виктор Кочетков <banana.development@yandex.ru>
2021-01-25 16:55:51 +0300

    test metadata

--- src/TeamCitySharp/DomainEntities/TestOccurrence.cs
+++ src/TeamCitySharp/DomainEntities/TestOccurrence.cs
@@ -3,64 +3,88 @@ using Newtonsoft.Json;
 
 namespace TeamCitySharp.DomainEntities
 {
-  public class TestOccurrence
-  {
-    public override string ToString()
+    public class TestMetadata
     {
-      return "testOccurrence";
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
     }
-    [JsonProperty("id")]
-    public string Id { get; set; }
 
-    [JsonProperty("name")]
-    public string Name { get; set; }
+    public class TestMetadataContainer
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
 
-    [JsonProperty("status")]
-    public string Status { get; set; }
+        [JsonProperty("typedValues")]
+        public List<TestMetadata> Metadata { get; set; }
+    }
 
-    [JsonProperty("ignored")]
-    public bool Ignored { get; set; }
+    public class TestOccurrence
+    {
+        public override string ToString()
+        {
+            return "testOccurrence";
+        }
+        [JsonProperty("id")]
+        public string Id { get; set; }
 
-    [JsonProperty("duration")]
-    public int Duration { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
-    [JsonProperty("runOrder")]
-    public string RunOrder { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
 
-    [JsonProperty("muted")]
-    public bool Muted { get; set; }
+        [JsonProperty("ignored")]
+        public bool Ignored { get; set; }
 
-    [JsonProperty("currentlyMuted")]
-    public bool CurrentlyMuted { get; set; }
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
 
-    [JsonProperty("currentlyInvestigated")]
-    public bool CurrentlyInvestigated { get; set; }
+        [JsonProperty("runOrder")]
+        public string RunOrder { get; set; }
 
-    [JsonProperty("href")]
-    public string Href { get; set; }
+        [JsonProperty("muted")]
+        public bool Muted { get; set; }
 
-    [JsonProperty("ignoreDetails")]
-    public string IgnoreDetails { get; set; }
+        [JsonProperty("currentlyMuted")]
+        public bool CurrentlyMuted { get; set; }
 
-    [JsonProperty("details")]
-    public string Details { get; set; }
+        [JsonProperty("currentlyInvestigated")]
+        public bool CurrentlyInvestigated { get; set; }
 
-    [JsonProperty("test")]
-    public Test Test { get; set; }
+        [JsonProperty("href")]
+        public string Href { get; set; }
 
-    [JsonProperty("mute")]
-    public Mute Mute { get; set; }
+        [JsonProperty("ignoreDetails")]
+        public string IgnoreDetails { get; set; }
 
-    [JsonProperty("build")]
-    public Build Build { get; set; }
+        [JsonProperty("details")]
+        public string Details { get; set; }
 
-    [JsonProperty("firstFailed")]
-    public Build FirstFailed { get; set; }
+        [JsonProperty("test")]
+        public Test Test { get; set; }
 
-    [JsonProperty("nextFixed")]
-    public Build NextFixed { get; set; }
+        [JsonProperty("mute")]
+        public Mute Mute { get; set; }
 
-    [JsonProperty("logAnchor")]
-    public string LogAnchor { get; set; }
-  }
+        [JsonProperty("build")]
+        public Build Build { get; set; }
+
+        [JsonProperty("firstFailed")]
+        public Build FirstFailed { get; set; }
+
+        [JsonProperty("nextFixed")]
+        public Build NextFixed { get; set; }
+
+        [JsonProperty("logAnchor")]
+        public string LogAnchor { get; set; }
+
+        [JsonProperty("newFailure")]
+        public bool? NewFailure { get; set; }
+
+        [JsonProperty("metadata")]
+        public TestMetadataContainer Metadata { get; set; }
+    }
 }
